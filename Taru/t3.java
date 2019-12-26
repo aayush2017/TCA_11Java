6937f8709c6940956f7de68b4f24808088811d7c
unknown <tarucs2014.academic@gmail.com>
2019-12-25 22:08:14 -0800

    t3 updated

---
+++ Taru/t3.java
@@ -0,0 +1,19 @@
+class RepeatElement  
+{ 
+    public static void main(String args[])  
+    { 
+	int a[]=new int[]{1,2,1,3,2,4,6,4};
+        int i, j; 
+        System.out.println("Repeated Elements are :"); 
+        for (i = 0; i < a.length; i++)  
+        { 
+            for (j = i+1; j < a.length; j++)  
+            { 
+                if (a[i] == a[j])  
+                    System.out.print("\n a="+a[i] + " "); 
+            } 
+        } 
+    } 
+  
+   
+} 
