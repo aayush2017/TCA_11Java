c8c0389f1c5fd4ebb9b0405e61e421fb8ebe5e20
unknown <tarucs2014.academic@gmail.com>
2019-12-29 22:19:09 -0800

    t3 not working

--- Taru/t3.java
+++ Taru/t3.java
@@ -2,15 +2,18 @@ class RepeatElement
 { 
     public static void main(String args[])  
     { 
-	int a[]=new int[]{1,2,1,3,2,4,6,4};
+	int a[]=new int[]{1,2,1,3,1,2,4,6,4,2};
         int i, j; 
         System.out.println("Repeated Elements are :"); 
         for (i = 0; i < a.length; i++)  
         { 
+
             for (j = i+1; j < a.length; j++)  
             { 
-                if (a[i] == a[j])  
-                    System.out.print("\n a="+a[i] + " "); 
+                if (a[i] == a[j])
+				{					
+			System.out.println(a[i]);
+				}					
             } 
         } 
     } 
